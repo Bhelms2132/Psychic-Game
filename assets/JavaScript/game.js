57c11601ed652562c75d8781a898ea3127f8f40d
bhelms2132 <bhelms2132@gmail.com>
2019-08-03 10:02:03 -0400

    wins losses working

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+//-- Letters chossen by computer. --//
+var computerChoice = ["a", "c", "g", "k", "r", "n"];
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+console.log(computerGuess);
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 5;
+var guessesSoFar = 0;
+
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessLeft-text");
+var guessesSoFar = document.getElementById("guessessofar");
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+    console.log(userGuess);
+    guessesLeft--;
+    console.log(guessesLeft);
+
+    
+
+    if (userGuess === computerGuess) {
+        wins++;
+        winsText.textContent = "wins: " + wins;
+        computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+        guessesLeft = 5;
+    }
+    else if (guessesLeft === 0){
+        losses++;
+        lossesText.textContent = "losses: " + losses;
+        computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+        guessesLeft = 5;
+
+    }
+}

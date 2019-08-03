e1f51d01f8d9146be7fb265dcd4b98d4a7d5b3ce
bhelms2132 <bhelms2132@gmail.com>
2019-08-03 19:32:13 -0400

    background image and Text cenetred in image

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -12,14 +12,14 @@ console.log(computerGuess);
 var wins = 0;
 var losses = 0;
 var guessesLeft = 5;
-var guessesSoFar = 0;
+var guessesSoFar = [];
 
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessLeft-text");
-var guessesSoFar = document.getElementById("guessessofar");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var guessesChosen = document.getElementById("guessessofar");
 
 document.onkeyup = function (event) {
 
@@ -27,20 +27,21 @@ document.onkeyup = function (event) {
     console.log(userGuess);
     guessesLeft--;
     console.log(guessesLeft);
-
+    guessesSoFar.push(userGuess);
+    console.log(guessesSoFar);
+    guessesLeftText.textContent = "guessesLeft: " + guessesLeft;
+    guessesChosen.textContent = "guessessofar: " + guessesSoFar;
     
-
     if (userGuess === computerGuess) {
         wins++;
         winsText.textContent = "wins: " + wins;
         computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
         guessesLeft = 5;
-    }
-    else if (guessesLeft === 0){
+    }else if (guessesLeft === 0){
         losses++;
         lossesText.textContent = "losses: " + losses;
         computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
         guessesLeft = 5;
 
-    }
+    } 
 }
